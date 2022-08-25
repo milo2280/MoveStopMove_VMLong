6a5625f695facfdbeb2312cbf68206c7fba8c0dc
milo2280 <longvm2280@gmail.com>
2022-08-25 18:01:54 +0700

    + Add: Character hold and throw weapon

---
+++ Assets/_Game/Scripts/Character/WeaponHolder.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponHolder : MonoBehaviour
+{
+    public Transform handTransform;
+    public Transform holderTransform;
+    public Transform bulletPoint;
+    public GameObject[] listWeaponPrefab;
+
+    private GameObject weaponObj;
+    private Weapon currentWeapon;
+
+    private void Start()
+    {
+        WeaponOnHand();
+        GenerateWeapon();
+    }
+
+    private void Update()
+    {
+        WeaponOnHand();
+    }
+
+    private void GenerateWeapon()
+    {
+        weaponObj = Instantiate(listWeaponPrefab[0], holderTransform);
+        currentWeapon = weaponObj.GetComponent<Weapon>();
+    }
+
+    private void WeaponOnHand()
+    {
+        holderTransform.position = handTransform.position;
+        holderTransform.rotation = handTransform.rotation;
+    }
+
+    public void Attack()
+    {
+        currentWeapon.Attack(bulletPoint.position, bulletPoint.rotation);
+    }
+}
--- Assets/_Game/Scripts/Weapon/Bullet.cs
+++ Assets/_Game/Scripts/Weapon/Bullet.cs
@@ -4,10 +4,28 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    public Rigidbody bulletRigidbody;
+    public Transform bulletTransform;
+    private float speed = 5f;
+
+
+    private void OnEnable()
+    {
+        OnInit();
+    }
+
     private void Update()
     {
         
     }
 
+    private void OnInit()
+    {
+        bulletRigidbody.velocity = bulletTransform.forward * speed;
+    }
 
+    private void OnDespawn()
+    {
+
+    }
 }
